8864c047db937dddef3978f1570460282912d724
Juan Coret <coret.juan@gmail.com>
2018-08-11 00:26:19 +0200

    you can now write, and the markdown will show correctly. need to fix layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
+import marked from 'marked';
+
+marked.setOptions({
+  breaks: true,
+  sanitize: true
+});
 
 class App extends Component {
+  constructor(props){
+    super(props);
+      this.state = {
+      markdown:'',
+      test:'',
+    }
+  }
+  handleChange(e){
+    // let newMarkdown = e.target.value.replace(/\r?\n/g, '<br />');
+    let newValue = e.target.value;
+    this.setState({
+      markdown: newValue,
+      test: marked(newValue.replace("\\n","\n")),
+    });
+  }
   render() {
+    
+    // let newValue = this.state.test.replace("\\n","<br/>")
+    console.log(this.state.test);
     return (
-        <p>Hello World!</p>
+     
+      <div id="container">
+        <div id="well">
+          <div id = "markdown" className="flex-content">
+              <textarea onChange={this.handleChange.bind(this)} value={this.state.markdown}></textarea>
+          </div>
+          <div id = "preview" className="flex-content" dangerouslySetInnerHTML = {{__html:this.state.test}}>
+            
+          </div>
+        </div>
+      </div>
 
     );
   }
